9ddcfc615557a701363f9edcc2af3ce4e65932aa
404 <113860914+TrafStaf@users.noreply.github.com>
2022-11-27 18:57:12 +0000

    all code

---
+++ main.js
@@ -0,0 +1,77 @@
+const log = console.log
+const char = {
+    x: 1,
+    y: 1
+}
+const arr = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
+const fieldHeight = () => arr.length - 1
+// function fieldHeight(){
+// return arr.lends
+// }
+const fieldWidth = () => arr[0].length - 1
+log(arr.length, fieldHeight(), fieldWidth())
+const field = document.querySelector('.field')
+arr.forEach(row => {
+    let htmlRow = '<div class="row">'
+    row.forEach(block => {
+        if (block == 0) htmlRow += '<div class="block empty"></div>'
+        else if (block == 1) htmlRow += '<div class="block solid"></div>'
+    })
+    htmlRow += '</div>'
+    field.innerHTML += htmlRow
+})
+
+function isCanStep() {
+    log(arr[char.y][char.x + 1])
+    return arr[char.y][char.x + 1] == 0
+}
+
+function isCanMove(direction) {
+    if (direction == 'right') return arr[char.y][char.x + 1] == 0
+    if (direction == 'left') return arr[char.y][char.x - 1] == 0
+    if (direction == 'up') return arr[char.y - 1][char.x] == 0
+    if (direction == 'down') return arr[char.y + 1][char.x] == 0
+}
+window.addEventListener('keypress', (e) => {
+    //arr[char.y][char.x+1]==0
+    if (e.code == 'KeyD' && char.x < fieldWidth() && isCanMove('right')) {
+        char.x++
+    }
+    if (e.code == 'KeyA' && char.x > 0 && isCanMove('left')) {
+        char.x--
+    }
+    if (e.code == 'KeyW' && char.y > 0 && isCanMove('up')) {
+        char.y--
+    }
+    if (e.code == 'KeyS' && char.y < fieldHeight() && isCanMove('down')) {
+        char.y++
+    }
+    goTo(char.x, char.y)
+})
+
+function goTo(x, y) {
+    const charDOM = document.querySelector('.char')
+    charDOM.style.left = x * 2 + 'rem'
+    charDOM.style.top = y * 2 + 'rem'
+}
+goTo(char.x, char.y)
